6410103edf8d8903325ca2828f3773f54004d69c
Leinad <leinad87@gmail.com>
2012-10-28 12:38:00 +0100

    getJSONfromURL

---
+++ src/net/asturdroid/lib/Network.java
@@ -0,0 +1,46 @@
+package net.asturdroid.lib;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+
+public class Network {
+
+	public static JSONObject getJSONfromURL(Context context, String url) throws ClientProtocolException, IOException, JSONException {
+
+		InputStream is = null;
+		String result = "";
+		JSONObject jArray = null;
+
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost(url);
+		HttpResponse response = httpclient.execute(httppost);
+		HttpEntity entity = response.getEntity();
+		is = entity.getContent();
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+		StringBuilder sb = new StringBuilder();
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			sb.append(line + "\n");
+		}
+		is.close();
+		result = sb.toString();
+
+		jArray = new JSONObject(result);
+
+		return jArray;
+	}
+}
